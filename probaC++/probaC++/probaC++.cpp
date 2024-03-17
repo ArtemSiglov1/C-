e93e2388803c004f82b9a0e16670b8378673aad4
ArtemSiglov1 <149941994+ArtemSiglov1@users.noreply.github.com>
2024-03-17 21:58:04 +0200

    Add files via upload

---
+++ probaC++/probaC++/probaC++.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+	//#include<cmath>
+	int main()
+	{
+		int a = 10;
+		int b = 15;
+		int c = 10;
+		int P = a + b + c;
+		double p = (double)P / 2;
+		double S = sqrt(p * (p - a) * (p - b) * (p - c));
+		std::cout << P << "\n" << S;
+
+	}
